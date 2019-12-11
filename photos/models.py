efd94e86ebbd28d75de44e07ec206cbe1f3e1381
gguerran <gustavoguerra.gr@gmail.com>
2019-12-11 15:21:22 -0300

    Remoção de importações não usadas

--- photos/models.py
+++ photos/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.conf import settings
-from taggit.managers import TaggableManager
-from django.contrib.auth.models import User
 
 
 class Photos(models.Model):
