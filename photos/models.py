3d9462abb21caede88b74a0c3911f60b5a6e95e9
gguerran <gustavoguerra.gr@gmail.com>
2019-12-11 14:46:30 -0300

    alterações - deploy pythonanywhere

--- photos/models.py
+++ photos/models.py
@@ -3,19 +3,22 @@ from django.conf import settings
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
 
+
 class Photos(models.Model):
     image = models.ImageField(
-    		upload_to = 'photos/media',
-    		verbose_name = 'Foto',
-    		null=False,
-    		blank=False
-    	)
+        upload_to='photos/media',
+        verbose_name='Foto',
+        null=False,
+        blank=False
+    )
     title = models.CharField('Título', max_length=200)
     quant_like = models.IntegerField('Curtidas', blank=True, default=0)
     created = models.DateTimeField('Enviada em', auto_now_add=True)
     aproved = models.BooleanField('Aprovada?', blank=True, default=False)
+
     def __str__(self):
         return self.title
+
     class Meta:
         verbose_name = 'Foto'
         verbose_name_plural = 'Fotos'
@@ -29,6 +32,7 @@ class Like(models.Model):
     author = models.IntegerField(
         'id_user', blank=False
     )
+
     class Meta:
         verbose_name = 'Curtida'
         verbose_name_plural = 'Curtidas'
